a37866bbe2e0d71a136ce6aa00209f5d3f305186
Brayan Garzon <destroyergarzon@gmail.com>
2024-12-16 12:15:04 -0500

    agregando las variables de entorno para conectar con la bd

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,49 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-# Inicializa la variable mongo para que sea accesible globalmente
-mongo = PyMongo()
-
+# Inicializa la base de datos
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuración de la URI de MongoDB Atlas
-    app.config["MONGO_URI"] = (
-        "mongodb+srv://brayangarzondev:WsYi8WI0cZsgeha6@chatbotbd.rko8t.mongodb.net/?retryWrites=true&w=majority&appName=ChatBotBD"
-    )
+    # Configuración de la URI de PostgreSQL usando la variable de entorno
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')  # Obtener la URL de la variable de entorno
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactiva el seguimiento de modificaciones (no necesario)
 
-    # Inicializa PyMongo con la aplicación
-    mongo.init_app(app)
+    # Inicializa SQLAlchemy con la aplicación
+    db.init_app(app)
 
     # Importar y registrar las rutas
     from .routes import main
-
     app.register_blueprint(main)
 
     return app
+
+
+
+# from flask import Flask
+# from flask_pymongo import PyMongo
+
+# # Inicializa la variable mongo para que sea accesible globalmente
+# mongo = PyMongo()
+
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     # Configuración de la URI de MongoDB Atlas
+#     app.config["MONGO_URI"] = (
+#         "mongodb+srv://brayangarzondev:WsYi8WI0cZsgeha6@chatbotbd.rko8t.mongodb.net/?retryWrites=true&w=majority&appName=ChatBotBD"
+#     )
+
+#     # Inicializa PyMongo con la aplicación
+#     mongo.init_app(app)
+
+#     # Importar y registrar las rutas
+#     from .routes import main
+
+#     app.register_blueprint(main)
+
+#     return app
