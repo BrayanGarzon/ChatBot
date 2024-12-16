4f68787e5604b06570ad7d1856744aacbcbe643a
Brayan Garzon <destroyergarzon@gmail.com>
2024-12-16 12:42:38 -0500

    cambios para  migrar a PostgreSQL correcion de importacion de models

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from app import db
-from models import Usuario  # Importa el modelo de usuario
+from app.models import Usuario  # Importa el modelo de usuario
 
 main = Blueprint("main", __name__)
 
